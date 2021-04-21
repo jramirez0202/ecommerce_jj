8fab12602fa0b0bcb42cdaa4cd60f33bdcd278f8
Johan Ramirez <johkcolom@gmail.com>
2021-04-20 22:16:22 -0400

    Se resuelve problemas de las rutas de devise y se configura admin para categories y user para futuras ordenes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
+  devise_for :admins 
   root to: 'home#index'
+  devise_for :users
+  
 
+
+  authenticate :admin do
+    resources :categories
+  end
 
   
-  resources :categories
   resources :products
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
